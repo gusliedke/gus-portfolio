218359f6f0c7f82a97e25cf69fd989d84f6ea9fc
Gustavo Liedke <contact@gustavoliedke.com>
2014-09-15 23:03:52 +0100

    first commit

---
+++ app/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'if08kXdr7LonjBf61bW8cuxPJ');
+    define('CONSUMER_SECRET', '1t2WZN8iSb9KK6odWrq69NOGvPnojryUAYkvtkWjp3XmkhFymH');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '170422082-HSkXAFjeWDyfkjjLmdVLn1aem7Qi5LYsecfNpdVr');
+    define('ACCESS_SECRET', 'Tjo4T5PkZN8AUWKx6IZx6WLnLZ0BV2tP36J9q9Z1SqV6T');
---
+++ app/scripts/main.js
@@ -0,0 +1,50 @@
+// burger menu animation
+
+var Menu = {
+	el: {
+		ham: $('.menu'),
+		menuTop: $('.menu-top'),
+		menuMiddle: $('.menu-middle'),
+		menuBottom: $('.menu-bottom'),
+		mainNav: $('#main-nav')
+	},
+
+	init: function() {
+		Menu.bindUIactions();
+	},
+
+	bindUIactions: function() {
+		Menu.el.ham
+		    .on(
+		      'click',
+		    function(event) {
+		    Menu.activateMenu(event);
+		    event.preventDefault();
+		}
+		);
+	},
+
+	activateMenu: function() {
+		Menu.el.menuTop.toggleClass('menu-top-click');
+		Menu.el.menuMiddle.toggleClass('menu-middle-click');
+		Menu.el.menuBottom.toggleClass('menu-bottom-click');
+		Menu.el.mainNav.toggleClass('active');
+	}
+};
+
+Menu.init();
+
+$(function(){
+	$('#twitter .tweet').twittie({
+	    dateFormat: '%b. %d, %Y',
+	    template: '{{tweet}} <div class="date">{{date}}</div>',
+	    count: 3
+	});
+});
+
+$(function(){
+	$("img.lazy").lazyload({
+    	container: $("#contain")
+	});
+})
+
